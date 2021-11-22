7fb89fd395a8b28a7835ba204d6fe715986da271
PS <ps@booghi.com>
2021-11-22 10:22:19 +0330

    Added initial models and migrations

--- restapitexample/main_panel/models.py
+++ restapitexample/main_panel/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    create_date= models.DateTimeField(auto_now_add=True)
+    update_date= models.DateTimeField(auto_now=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    create_date= models.DateTimeField(auto_now_add=True)
+    update_date= models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    book = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.ManyToManyField(Genre)
+    isbn = models.CharField(max_length=12, unique=True)
+    description = models.TextField(max_length=1500)
+    create_date= models.DateTimeField(auto_now_add=True)
+    update_date= models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    reply_to = models.ForeignKey('self', on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=500)
+    create_date= models.DateTimeField(auto_now_add=True)
+    update_date= models.DateTimeField(auto_now=True)
